b3501a19a28bfccd9fc3fd279848208c0dbdd34a
meh <dontsellfish@gmail.com>
2023-01-17 15:47:25 +0300

    config from console

--- main.go
+++ main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"channel_bot/channelbot"
+	"flag"
 	"log"
 )
 
+const DefaultConfigPath = "./cfg.json"
+
 func main() {
-	bot, err := channelbot.FromFile("octorat.json")
+	config := flag.String("c", DefaultConfigPath, "path to a config file")
+	flag.Parse()
+
+	bot, err := channelbot.FromFile(*config)
 	if err != nil {
 		log.Fatalln(err)
 	}
